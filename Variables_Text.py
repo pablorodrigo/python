e0623cbf6ee354745ff004b6e720ac99079950c2
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2019-02-24 20:43:48 -0400

    learning variables

---
+++ Variables_Text.py
@@ -0,0 +1,14 @@
+# Python language basic 1
+# variables syntax
+# variable types: ints, floats, booleans, strings
+
+x_pos = 5
+speed = 2.5
+is_game_over = False
+character_name = 'Pablo'
+
+print(type(x_pos))
+
+x_pos = 'text'
+
+print(x_pos)
