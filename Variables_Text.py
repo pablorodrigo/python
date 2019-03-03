4e3fa59934f789002dbd646451845ae216c58ce5
pablorodrigo <wfpablo@hotmail.com>
2019-03-03 19:09:51 -0400

    learning collections

--- Variables_Text.py
+++ Variables_Text.py
@@ -2,13 +2,29 @@
 # variables syntax
 # variable types: ints, floats, booleans, strings
 
-x_pos = 5
-speed = 2.5
-is_game_over = False
-character_name = 'Pablo'
+x_pos = 5    # int
+speed = 2.5    # float
+is_game_over = False    # boolean
+character_name = 'Pablo'    # string
 
 print(type(x_pos))
 
 x_pos = 'text'
 
 print(x_pos)
+
+# Python language basics 2
+# operations
+# assignment = not
+# arithmetic: + - * / % // += -= *= /= **
+# conditional: > >= < <= != ==
+
+
+new_x_pos = x_pos + speed # 5
+
+full_name = character_name + ' Rodrigo'
+
+x_pos = 5
+mod_x_pos = 5 %     # 1
+floor_div_x_pos = x_pos // 2    # 2
+x_pos_squared = x_pos**2    # 25
